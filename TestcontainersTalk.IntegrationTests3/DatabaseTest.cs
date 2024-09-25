5571fb982c0eb61c336ea78c530dc061bed7fe85
Daniel Ward <dward256@hotmail.com>
2024-09-24 23:35:17 -0500

    protected methods

--- TestcontainersTalk.IntegrationTests3/DatabaseTest.cs
+++ TestcontainersTalk.IntegrationTests3/DatabaseTest.cs
@@ -10,20 +10,20 @@ public abstract class DatabaseTest : IAsyncLifetime
     protected readonly BookstoreContext DbContext;
     private readonly Func<Task> _resetDatabase;
 
-    public DatabaseTest(IntegrationTestFactory factory)
+    protected DatabaseTest(IntegrationTestFactory factory)
     {
         _resetDatabase = factory.ResetDatabase;
         DbContext = factory.Db;
         Client = factory.CreateClient();
     }
 
-    public async Task Insert<T>(T entity) where T : class
+    protected async Task Insert<T>(T entity) where T : class
     {
         await DbContext.AddAsync(entity);
         await DbContext.SaveChangesAsync();
     }
     
-    public async Task InsertMany<T>(IEnumerable<T> entities) where T : class
+    protected async Task InsertMany<T>(IEnumerable<T> entities) where T : class
     {
         await DbContext.AddRangeAsync(entities);
         await DbContext.SaveChangesAsync();
